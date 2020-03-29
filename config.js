cd7352e31b777798e02c1f0a09dfaa6622f8475d
Luis Martinez <mrluismartinezzz@gmail.com>
2020-03-29 13:53:42 -0700

    config 4 anthony

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ const noConfig = variable => {
 // Set the corresponding environment variables, or replace the values here before using it in your environment.
 module.exports = {
     TITLE: 'Metaphysics' || noConfig('TITLE'), // Required
-    AUTHOR: 'The End Science' || noConfig('AUTHOR'), // Required
+    AUTHOR: 'Dr. Anthony Long' || noConfig('AUTHOR'), // Required
     URL: 'https://www.theendsciencemetaphysics.com' || noConfig('URL'), // Required
-    EMAIL: 'mrluismartinezzz@gmail.com' || noConfig('EMAIL'), // Required
-    DESCRIPTION: 'blog posts about Metaphysics' || noConfig('DESCRIPTION'), // Required
+    EMAIL: 'drlong18@gmail.com' || noConfig('EMAIL'), // Required
+    DESCRIPTION: 'Invoking thoughts about reality' || noConfig('DESCRIPTION'), // Required
     FATHOMSITEID: 'FATHOMSITEID' || noConfig('FATHOMSITEID'), // Required
     DISQUSSHORTNAME: 'theendsciencemetaphysics' || noConfig('DISQUSSHORTNAME'), // Required
 };
